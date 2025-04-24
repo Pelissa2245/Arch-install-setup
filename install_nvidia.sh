50c73fb011d3807e19339d1b230de74eba11f63f
Matheus Pelissari <163481967+Pelissa2245@users.noreply.github.com>
2025-04-24 07:10:08 -0300

    install_nvidia.sh

--- install_nvidia.sh
+++ install_nvidia.sh
@@ -4,31 +4,3 @@ sudo pacman -Syu --noconfirm
 
 sudo pacman -S --noconfirm nvidia nvidia-utils nvidia-settings opencl-nvidia xorg-server-devel
 
-mkdir -p /etc/X11/xorg.conf.d
-
-cat >> /etc/X11/xorg.conf.d/10-nvidia-drm-outputclass.conf<<EOF
-Section "OutputClass"
-    Identifier "intel"
-    MatchDriver "i915"
-    Driver "modesetting"
-EndSection
-
-Section "OutputClass"
-    Identifier "nvidia"
-    MatchDriver "nvidia-drm"
-    Driver "nvidia"
-    Option "AllowEmptyInitialConfiguration"
-    Option "PrimaryGPU" "yes"
-    ModulePath "/usr/lib/nvidia/xorg"
-    ModulePath "/usr/lib/xorg/modules"
-EndSection
-EOF
-
-mkdir -p /etc/modprobe.d
-
-cat >> /etc/modprobe.d/nvidia-drm-nomodeset.conf<<EOF
-options nvidia-drm modeset=1
-EOF
-
-sudo mkinitcpio -P
-
